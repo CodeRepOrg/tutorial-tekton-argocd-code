7d2034dd369954c90e06108b51201fafd4181bad
JoaoPedroPP <jp052@hotmail.com>
2021-03-30 15:06:07 -0300

    test for pipelines

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Cloud Native demo March 12th 2020' });
+  res.render('index', { title: 'Cloud Native demo March 12th 2020', authors: 'This was made possible hue squad 6!!!!' });
 });
 
 module.exports = router;
