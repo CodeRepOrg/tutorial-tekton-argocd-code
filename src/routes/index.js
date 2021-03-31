e3ae2c1e412d23a6621d9815e628a71232088208
JoaoPedroPP <jp052@hotmail.com>
2021-03-31 10:37:48 -0300

    added ! in h1

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'IBM Cloud Native demo March 31st 2020', authors: 'This was made by possible hue squad 6!!!!' });
+  res.render('index', { title: 'IBM Cloud Native demo March 31st 2020!', authors: 'This was made by possible hue squad 6!!!!' });
 });
 
 module.exports = router;
